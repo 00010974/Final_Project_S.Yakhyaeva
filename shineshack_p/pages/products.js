6dd6b406094315f1929cf469ccc709d3cac981f3
00010974 <sabrina290103@gmail.com>
2024-04-06 20:40:48 +0500

    .

--- shineshack_p/pages/products.js
+++ shineshack_p/pages/products.js
@@ -2,6 +2,8 @@ import Layout from "@/components/Layout";
 
 export default function Products(){
     return (
-        <Layout>products</Layout>
+        <Layout>
+            <Link classname="bg-green-700 text-white rounded-md py-1 px-2" href={'/product/new'}>Add new product</Link>
+        </Layout>
     )
 }
