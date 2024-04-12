6c68c5a11fb313688e92bd8642fd399aa35f537a
00010974 <sabrina290103@gmail.com>
2024-04-13 01:38:56 +0500

    ю

--- shineshack_p/pages/products.js
+++ shineshack_p/pages/products.js
@@ -1,8 +1,12 @@
 //password mangodb atlas k7XVrWMyWqh$jqJ mail sbyakhyaeva@gmail.com/sabrina290103@gmail.com
 import Link from 'next/link';
 import Layout from "@/components/Layout";
+import { useEffect } from 'react';
 
 export default function Products(){
+    useEffect(() => {
+
+    }, []);
     return (
         <Layout>
             <Link className="bg-green-700 text-white rounded-md py-1 px-2" href={'/Products/new_product'}>Add new product</Link>
