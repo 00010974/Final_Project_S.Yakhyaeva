0a869cdc5eb1ba6c971cc179e799a8c1cdb2e410
00010974 <sabrina290103@gmail.com>
2024-04-16 02:20:49 +0500

    upload photos

--- shineshack_p/pages/Products/new_product.js
+++ shineshack_p/pages/Products/new_product.js
@@ -2,35 +2,14 @@ import Layout from "@/components/Layout";
 import {useState} from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import ProductForm from "@/components/ProductForm";
 
 export default function newProduct(){
-    const [title, setTitle] = useState('');
-    const [desctiption, setDesctiption] = useState('');
-    const [price, setPrice] = useState('');
-    const [goToProducts, setGoToProducts] = useState(false);
-    const router = useRouter();
-    async function createProduct(ev){
-        ev.preventDefault();
-        const data = {title, desctiption, price};
-        // to create a product we need to send a post request to the api so i use an axios
-        await axios.post('/api/products', data);
-        setGoToProducts(true);
-    }
-    if (goToProducts){
-        router.push('/products');
-    }
-    return(
-        <Layout>
-            <form onSubmit={createProduct}>
-                <h1>New Product</h1>
-                <label>Product Name</label>
-                <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
-                <label>Desctiption</label>
-                <textarea placeholder="desctiption" value={desctiption} onChange={ev => setDesctiption(ev.target.value)}/>
-                <label>Price (in USD)</label>
-                <input type="number" placeholder="price" value={price} onChange={ev => setPrice(ev.target.value)}/>
-                <button type="submit" className="btn-primary">Save</button>
-            </form>
-        </Layout>
-    )
+    
+    return (
+    <Layout>
+        <h1>New Product</h1>
+        <ProductForm />
+    </Layout>
+    );
 }
